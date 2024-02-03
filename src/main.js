4cffd6ab4cb00864cadfa4a1ff33d53165d82315
Irismiranda <110883702+Irismiranda@users.noreply.github.com>
2024-02-03 20:57:52 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -62,6 +62,39 @@ function renderMenu(){
     }
 }
 
+function fetchColorPalette(selectedColor, selectedPaletteMode, colorCount){
+    const fetchLink = `https://www.thecolorapi.com/scheme?hex=${selectedColor}&mode=${selectedPaletteMode}&named=true&count=${colorCount}`
+    fetch(fetchLink, {method: 'GET'})
+        .then(res => res.json())
+        .then(colorDataArr => {
+            let colorPaletteHtml = ``
+             colorDataArr.colors.forEach(color => {
+                 const hexValue = color.hex.value
+                 const colorName = color.name.value
+                 colorPaletteHtml += `
+                    <div class='color-container' style='background-color:${hexValue}'> 
+                        <p class='copy-message invisible' id='${hexValue}'> Copied </p>
+                        <div class='color-code'>
+                            <div class='color-data-container'>
+                                <p> ${hexValue} </p>
+                               <div class='flex center'> 
+                                    <p class='bold'> ${colorName} </p> 
+                                    <div class='clipboard' data-NewColorHex="${hexValue}">
+                                        <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 115.77 122.88" style="enable-background:new 0 0 115.77 122.88" xml:space="preserve">
+                                        <style type="text/css">.st0{fill-rule:evenoddclip-rule:evenodd}</style><g><path class="st0" d="M89.62,13.96v7.73h12.19h0.01v0.02c3.85,0.01,7.34,1.57,9.86,4.1c2.5,2.51,4.06,5.98,4.07,9.82h0.02v0.02 v73.27v0.01h-0.02c-0.01,3.84-1.57,7.33-4.1,9.86c-2.51,2.5-5.98,4.06-9.82,4.07v0.02h-0.02h-61.7H40.1v-0.02 c-3.84-0.01-7.34-1.57-9.86-4.1c-2.5-2.51-4.06-5.98-4.07-9.82h-0.02v-0.02V92.51H13.96h-0.01v-0.02c-3.84-0.01-7.34-1.57-9.86-4.1 c-2.5-2.51-4.06-5.98-4.07-9.82H0v-0.02V13.96v-0.01h0.02c0.01-3.85,1.58-7.34,4.1-9.86c2.51-2.5,5.98-4.06,9.82-4.07V0h0.02h61.7 h0.01v0.02c3.85,0.01,7.34,1.57,9.86,4.1c2.5,2.51,4.06,5.98,4.07,9.82h0.02V13.96L89.62,13.96z M79.04,21.69v-7.73v-0.02h0.02 c0-0.91-0.39-1.75-1.01-2.37c-0.61-0.61-1.46-1-2.37-1v0.02h-0.01h-61.7h-0.02v-0.02c-0.91,0-1.75,0.39-2.37,1.01 c-0.61,0.61-1,1.46-1,2.37h0.02v0.01v64.59v0.02h-0.02c0,0.91,0.39,1.75,1.01,2.37c0.61,0.61,1.46,1,2.37,1v-0.02h0.01h12.19V35.65 v-0.01h0.02c0.01-3.85,1.58-7.34,4.1-9.86c2.51-2.5,5.98-4.06,9.82-4.07v-0.02h0.02H79.04L79.04,21.69z M105.18,108.92V35.65v-0.02 h0.02c0-0.91-0.39-1.75-1.01-2.37c-0.61-0.61-1.46-1-2.37-1v0.02h-0.01h-61.7h-0.02v-0.02c-0.91,0-1.75,0.39-2.37,1.01 c-0.61,0.61-1,1.46-1,2.37h0.02v0.01v73.27v0.02h-0.02c0,0.91,0.39,1.75,1.01,2.37c0.61,0.61,1.46,1,2.37,1v-0.02h0.01h61.7h0.02 v0.02c0.91,0,1.75-0.39,2.37-1.01c0.61-0.61,1-1.46,1-2.37h-0.02V108.92L105.18,108.92z"/>
+                                        </g>
+                                        </svg>
+                                    </div>
+                               </div>
+                            </div>
+                        </div>
+                    </div>
+                 `
+            })
+            document.getElementById('main-div').innerHTML = colorPaletteHtml
+    })
+}
+
 function getNewValues(){
     colorCountInput.value = `${colorCount} colors`
     fetchColorPalette(colorPicker.value.slice(1), getSelectedPaletteMode().toLocaleLowerCase(), colorCount)
@@ -81,28 +114,32 @@ function hideDropDown(){
     arrowIcon.classList.remove('arrow-icon-active')  
 }
 
-function copyToClipboard(hexValue){
-    const textArea = document.createElement("textarea")
-    textArea.value = hexValue
+function copyToClipboard(e){
+    const clipboardElement = e.target.closest('.clipboard');
 
-    document.body.appendChild(textArea)
+    if (clipboardElement) {
+        const currentHexCode = e.target.parentElement.getAttribute("data-NewColorHex")
+        const textArea = document.createElement("textarea")
+        textArea.value = currentHexCode
 
-    const range = document.createRange()
-    range.selectNode(textArea)
-    window.getSelection().removeAllRanges()
-    window.getSelection().addRange(range)
-    try {
-        document.execCommand('copy')
-        document.getElementById(hexValue).classList.remove('invisible')
-        setTimeout(function () {
-            document.getElementById(hexValue).classList.add('invisible')
-        }, 2000)
-    } catch (err) {
-        console.error('Unable to copy to clipboard', err)
-    } finally {
-        document.body.removeChild(textArea)
+        document.body.appendChild(textArea)
+
+        const range = document.createRange()
+        range.selectNode(textArea)
+        window.getSelection().removeAllRanges()
+        window.getSelection().addRange(range)
+        try {
+            document.execCommand('copy')
+            document.getElementById(currentHexCode).classList.remove('invisible')
+            setTimeout(function () {
+                document.getElementById(currentHexCode).classList.add('invisible')
+            }, 2000)
+        } catch (err) {
+            console.error('Unable to copy to clipboard', err)
+        } finally {
+            document.body.removeChild(textArea)
+        }
     }
-
 }
 
 getRandomColor()
@@ -120,6 +157,7 @@ dropDown.addEventListener('click', function(e) {
     const target = e.target
 
     if (e.target.className.includes('select-items')) {
+        const currentHexCode = e.target.getAttribute("data-NewColorHex")
         const id = target.getAttribute('data-id')
         handleSelectorClick(id)
     }
@@ -152,40 +190,9 @@ countDownArrow.addEventListener('click', function(){
 })
 
 mainDiv.addEventListener('click', function(e){
-    hideDropDown()
+    copyToClipboard(e)
 })
 
-function fetchColorPalette(selectedColor, selectedPaletteMode, colorCount){
-    const fetchLink = `https://www.thecolorapi.com/scheme?hex=${selectedColor}&mode=${selectedPaletteMode}&named=true&count=${colorCount}`
-    fetch(fetchLink, {method: 'GET'})
-        .then(res => res.json())
-        .then(colorDataArr => {
-            let colorPaletteHtml = ``
-             colorDataArr.colors.forEach(color => {
-                 const hexValue = color.hex.value
-                 const colorName = color.name.value
-                 colorPaletteHtml += `
-                    <div class='color-container' style='background-color:${hexValue}'> 
-                        <p class='copy-message invisible' id='${hexValue}'> Copied </p>
-                        <div class='color-code'>
-                            <div class='color-data-container'>
-                                <p> ${hexValue} </p>
-                               <div class='flex center'> 
-                                    <p class='bold'> ${colorName} </p> 
-                                    <div 
-                                    onclick="copyToClipboard('${hexValue}')"
-                                    class='clipboard'>
-                                        <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 115.77 122.88" style="enable-background:new 0 0 115.77 122.88" xml:space="preserve">
-                                        <style type="text/css">.st0{fill-rule:evenoddclip-rule:evenodd}</style><g><path class="st0" d="M89.62,13.96v7.73h12.19h0.01v0.02c3.85,0.01,7.34,1.57,9.86,4.1c2.5,2.51,4.06,5.98,4.07,9.82h0.02v0.02 v73.27v0.01h-0.02c-0.01,3.84-1.57,7.33-4.1,9.86c-2.51,2.5-5.98,4.06-9.82,4.07v0.02h-0.02h-61.7H40.1v-0.02 c-3.84-0.01-7.34-1.57-9.86-4.1c-2.5-2.51-4.06-5.98-4.07-9.82h-0.02v-0.02V92.51H13.96h-0.01v-0.02c-3.84-0.01-7.34-1.57-9.86-4.1 c-2.5-2.51-4.06-5.98-4.07-9.82H0v-0.02V13.96v-0.01h0.02c0.01-3.85,1.58-7.34,4.1-9.86c2.51-2.5,5.98-4.06,9.82-4.07V0h0.02h61.7 h0.01v0.02c3.85,0.01,7.34,1.57,9.86,4.1c2.5,2.51,4.06,5.98,4.07,9.82h0.02V13.96L89.62,13.96z M79.04,21.69v-7.73v-0.02h0.02 c0-0.91-0.39-1.75-1.01-2.37c-0.61-0.61-1.46-1-2.37-1v0.02h-0.01h-61.7h-0.02v-0.02c-0.91,0-1.75,0.39-2.37,1.01 c-0.61,0.61-1,1.46-1,2.37h0.02v0.01v64.59v0.02h-0.02c0,0.91,0.39,1.75,1.01,2.37c0.61,0.61,1.46,1,2.37,1v-0.02h0.01h12.19V35.65 v-0.01h0.02c0.01-3.85,1.58-7.34,4.1-9.86c2.51-2.5,5.98-4.06,9.82-4.07v-0.02h0.02H79.04L79.04,21.69z M105.18,108.92V35.65v-0.02 h0.02c0-0.91-0.39-1.75-1.01-2.37c-0.61-0.61-1.46-1-2.37-1v0.02h-0.01h-61.7h-0.02v-0.02c-0.91,0-1.75,0.39-2.37,1.01 c-0.61,0.61-1,1.46-1,2.37h0.02v0.01v73.27v0.02h-0.02c0,0.91,0.39,1.75,1.01,2.37c0.61,0.61,1.46,1,2.37,1v-0.02h0.01h61.7h0.02 v0.02c0.91,0,1.75-0.39,2.37-1.01c0.61-0.61,1-1.46,1-2.37h-0.02V108.92L105.18,108.92z"/>
-                                        </g>
-                                        </svg>
-                                    </div>
-                               </div>
-                            </div>
-                        </div>
-                    </div>
-                 `
-            })
-            document.getElementById('main-div').innerHTML = colorPaletteHtml
-    })
-}
+mainDiv.addEventListener('click', function(){
+    hideDropDown()
+})
